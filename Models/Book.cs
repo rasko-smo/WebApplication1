95f1bdea635839275f3b4401833dc9fb0a835dd3
rasko-smo <smo230918@gmail.com>
2026-01-11 21:40:31 +0900

    Bookの表示名を新規作成

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,13 +1,21 @@
-﻿namespace WebApplication1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
 {
     public class Book
     {
         public int Id { get; set; }
+        [Display(Name = "ISBN")]
         public string Isbn { get; set; } = String.Empty;
+        [Display(Name = "タイトル")]
         public string Title { get; set; } = String.Empty;
+        [Display(Name = "価格")]
         public int Price { get; set; }
+        [Display(Name = "出版社")]
         public string Publisher { get; set; } = String.Empty;
+        [Display(Name = "刊行日")]
         public DateTime Published { get; set; }
+        [Display(Name = "配布サンプル")]
         public bool Sample { get; set; }
     }
 }
